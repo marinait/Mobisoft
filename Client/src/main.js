d1ed5fd224528d5bf0bbab4409767b151358431e
marinait <marinajob@gmail.com>
2019-08-04 01:46:15 +0300

    Update main.js

--- Client/src/main.js
+++ Client/src/main.js
@@ -1,47 +1,43 @@
 import React, { Component } from "react";
-import './main.css';
-import Login from './login/login';
-import Logout from './login/logout';
-import Products from './products/products';
-import Twofa from './login/twofa';
-import {authenticationService} from './services/authenticationService';
+import "./main.css";
+import Login from "./login/login";
+import Logout from "./login/logout";
+import Products from "./products/products";
+import Twofa from "./login/twofa";
+import { authenticationService } from "./services/authenticationService";
 
 export default class Main extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentUser: null
-        };
-    }
+    this.state = {
+      currentUser: null
+    };
+  }
 
   componentDidMount() {
-    authenticationService.currentUser.subscribe(x => this.setState({ currentUser: x }));
+      //we have SPA which we manage via this.state.currentUser
+      //it's the user updated after login
+    authenticationService.currentUser.subscribe(x =>
+      this.setState({ currentUser: x })
+    );
+    //if we have token (in localstorage) but it is not valid (outdated) then log out 
     authenticationService.testToken();
   }
 
   render() {
-     return (  
-      <div>   
-      {this.state.currentUser ?
-         this.state.currentUser.verified ?
-      ([<Products key="1"/>, <Logout key="2"/>])
-         :
-      ([<Twofa key="1"/>, <Logout key="2"/>])
-         :
-         (<Login/>)
-      }
-      </div>)
-     /*{if currentUser && currentUser.value.verified &&
-          [<Products/>, <Logout/>]
-     }
-     {currentUser && !currentUser.value.verified &&
-        [<Twofa/>, <Logout/>]
-     }
-    {!currentUser &&
-        <Login/>
-     }
-     (/
-  }*/
+    return (
+      <div>
+        {this.state.currentUser ? (
+          this.state.currentUser.verified ? (
+            [<Products key="1" />, <Logout key="2" />]
+          ) : (
+            [<Twofa key="1" />, <Logout key="2" />]
+          )
+        ) : (
+          <Login />
+        )}
+      </div>
+    );
   }
 }
