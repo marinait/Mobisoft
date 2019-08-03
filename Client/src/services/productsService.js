19ead27e154bef6f3c578be52f79b92b495db2b0
marinait <marinajob@gmail.com>
2019-08-04 02:08:37 +0300

    Update productsService.js

--- Client/src/services/productsService.js
+++ Client/src/services/productsService.js
@@ -1,67 +1,72 @@
-import {authenticationService} from './authenticationService';
+import { authenticationService } from "./authenticationService";
+import {config} from '../config';
 
 export const productsService = {
-    getAllProducts,
-    getBasket,
-    clearBasket,
-    addToBasket
+  getAllProducts,
+  getBasket,
+  clearBasket,
+  addToBasket
 };
 
-function getAuthorizedPOSTRequestOptions(){
-    return {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', 
-                   'Authorization': 'Bearer ' +  authenticationService.currentUserValue.token},
-        body: ''            
-    };
+function getAuthorizedPOSTRequestOptions() {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + authenticationService.currentUserValue.token
+    },
+    body: ""
+  };
 }
-function getAllProducts(){
-    let requestOptions = getAuthorizedPOSTRequestOptions();
+function getAllProducts() {
+  let requestOptions = getAuthorizedPOSTRequestOptions();
 
-    return fetch(`http://localhost:8003/api/getallproducts`, requestOptions)
-   .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).catch((error) =>{
-        console.log(error);
-    });
-
-}
-
-function getBasket(userId){
-    let requestOptions = getAuthorizedPOSTRequestOptions();
-    requestOptions.body = JSON.stringify({ userId });
-    return fetch(`http://localhost:8003/api/getBasketByUserId`, requestOptions)
+  return fetch(`${config.serverUrl}/api/getallproducts`, requestOptions)
     .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).catch((error) =>{
-        console.log(error);
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .catch(error => {
+      console.log(error);
     });
 }
 
-function clearBasket(userId){
-    let requestOptions = getAuthorizedPOSTRequestOptions();
-    requestOptions.body = JSON.stringify({ userId });
-    return fetch(`http://localhost:8003/api/clearBasketByUserId`, requestOptions)
-
+function getBasket(userId) {
+  let requestOptions = getAuthorizedPOSTRequestOptions();
+  requestOptions.body = JSON.stringify({ userId });
+  return fetch(`${config.serverUrl}/api/getBasketByUserId`, requestOptions)
     .then(response => {
-        if (response.ok) {
-            return response.json();
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function clearBasket(userId) {
+  let requestOptions = getAuthorizedPOSTRequestOptions();
+  requestOptions.body = JSON.stringify({ userId });
+  return fetch(
+    `${config.serverUrl}/api/clearBasketByUserId`,
+    requestOptions
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
     }
   });
 }
 
-function addToBasket(userId, productId){
-    let requestOptions = getAuthorizedPOSTRequestOptions();
-    requestOptions.body = JSON.stringify({ userId, productId });
-    return fetch(`http://localhost:8003/api/addToBasket`, requestOptions)
-
-    .then(response => {
-        if (response.ok) {
-            return response.json();
+function addToBasket(userId, productId) {
+  let requestOptions = getAuthorizedPOSTRequestOptions();
+  requestOptions.body = JSON.stringify({ userId, productId });
+  return fetch(`${config.serverUrl}/api/addToBasket`, requestOptions).then(
+    response => {
+      if (response.ok) {
+        return response.json();
+      }
     }
-  });
+  );
 }
